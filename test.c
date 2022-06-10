0cc21b8fd20145620a0b9565754e37caf7af85c9
EternalNico <95299241+EternalNico@users.noreply.github.com>
2022-06-10 17:42:51 +0200

    Update main.c

--- test.c
+++ test.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-#include "solution.h"
+#include <stdlib.h>
+#include <string.h>
+#include "solution1.c"
+#include "solution2.c"
+
 int main() {
-    printf("%s\n",erase("Cou cou  J M  B"));
+    //Test 1
+  printf("%s\n",erase("Cou cou  J M  B"));
+
+   //Test 2
+  char chaine[] = "Cou cou  J M  B"; 
+  printf("'%s'\n",erase2(chaine));
+  return 0;
 }
