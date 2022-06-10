26296dc039742da9bbd3dc7a6026bd975b43f2a5
EternalNico <95299241+EternalNico@users.noreply.github.com>
2022-06-10 17:49:23 +0200

    Update solution2.c Documentation

--- solution2.c
+++ solution2.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include "solution.h"
 
-
+/** Permet de supprimer seulement les espaces individuels d'une chaîne de caractère
+ * @param IN chaine : La chaîne de caractère à tester
+ *@return la chaine
+**/
 char* erase2(char* chaine) {
   //Déclaration des attributs neécessaires
   int j = 0;
