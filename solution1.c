2751a990583891dd739aeef64258498ac2b0021c
EternalNico <95299241+EternalNico@users.noreply.github.com>
2022-06-10 17:45:08 +0200

    Update : solution1.c

--- solution1.c
+++ solution1.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "solution.h"
+
 
 char* erase(char* chaine) {
-    return "fail";
+  //Déclaration des attributs neécessaires
+  char* ChaineSE = calloc(strlen(chaine),sizeof(int)), i = 0, j = 0;
+   
+   while (chaine[i] != '\0')//Tant que je n'atteint pas la dernière case du tableau
+   {
+       //Si il y a des espaces consécutifs dans la chaines de caractères
+      if ((chaine[i] == ' ' && chaine[i+1] != ' ' && chaine[i-1] != ' ') != 1) {
+        ChaineSE[j] = chaine[i];
+        j++;
+      }
+      i++;
+   }
+   
+   //Dernière case du tableau
+   ChaineSE[j] = '\0';
+
+   //Affichage de la chaîne de caractère sans les espaces simples 
+   printf("Après la suppression des espaces :'%s' ", ChaineSE);
+   
+   //Libération de la mémoire
+   free(ChaineSE);
 }
+
